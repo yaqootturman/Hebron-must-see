e07cf06c0cb46cfcc032337dbc8588be5cd49a17
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-09-01 14:08:42 +0300

    setup server related #30

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const router = require("./router");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(router);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
+module.exports = app;
