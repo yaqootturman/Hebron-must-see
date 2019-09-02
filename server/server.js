d50d4ed982b924a1a0fda67ed557aa3f321e4da7
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-09-02 11:23:20 +0300

    add eslint config to backend #31 - eslint backend files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const PORT = process.env.PORT || 4000;
-const app = require("./app");
+const PORT = process.env.PORT || 4000
+const app = require('./app')
 
 app.listen(PORT, () => {
-  console.log(`localhost: ${PORT}`);
-});
+  console.log(`localhost: ${PORT}`)
+})
