fa1bb2e83e4873b4be727552dc955906956ea843
Rand Inaim <rand.inaim95@gmail.com>
2019-09-04 14:15:09 +0300

    simplify filter and start component

--- client/src/components/Filter/index.js
+++ client/src/components/Filter/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import './style.css'
 
-const Filter = ({ filterList, updateClickedFilter }) => {
-	return (
-		<React.Fragment>
-			{filterList.map((filterType) => {
-				return <button onClick={() => updateClickedFilter(filterType)}>{filterType}</button>
-			})}
-		</React.Fragment>
-	)
-}
+const Filter = ({ filterList, updateClickedFilter }) => (
+	<React.Fragment>
+		{filterList.map((filterType) => {
+			return <button onClick={() => updateClickedFilter(filterType)}>{filterType}</button>
+		})}
+	</React.Fragment>
+)
 
 export default Filter
--- client/src/components/Start/index.js
+++ client/src/components/Start/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './style.css'
 
-const Start = () => {
-	return (
-		<React.Fragment>
-			<Link to="/places">places</Link>
-			<Link to="/guides">guides</Link>
-		</React.Fragment>
-	)
-}
+const Start = () => (
+	<React.Fragment>
+		<Link to="/places">places</Link>
+		<Link to="/guides">guides</Link>
+	</React.Fragment>
+)
 
 export default Start
