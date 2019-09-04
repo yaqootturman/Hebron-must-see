448d2c108bc35d7ee949936c45bd68d8b3062afb
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-09-04 10:07:29 +0300

    the app contains signup component related to #20

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Start from './components/Start'
+import Signup from './components/SharedComponents/Signup'
+
 import './App.css'
 
 const App = () => {
-	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" component={Start} />
-			</Switch>
-		</Router>
-	)
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Start} />
+          <Route exact path="/signup" component={Signup} />
+        </Switch>
+      </Router>
+    </>
+  )
 }
 
 export default App
