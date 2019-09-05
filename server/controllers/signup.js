d6658a86027213dd41fe30d0dea3b5f184e073af
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-09-05 16:42:08 +0300

    edit signup server

--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -1,19 +1,55 @@
 const bcrypt = require('bcrypt')
 const addUser = require('../database/queries/addUser')
+const addGuide = require('../database/queries/addGuide')
 
 exports.signUp = (request, response) => {
-  const { email, name, password } = request.body
+  const { email, name, password, userType } = request.body
+  console.log(email, name, password, userType, 'gggggg')
 
   bcrypt.hash(password, 10, (err, hash) => {
     if (err) {
       console.log(err)
     }
-    addUser(email, name, hash, (err, res) => {
-      if (err) {
-        console.log(err)
-      } else {
-        response.redirect('/')
-      }
-    })
+    if (userType === 'user') {
+      console.log('user')
+      addUser(email, name, hash, (err, res) => {
+        if (err) {
+          console.log(err)
+        } else {
+          response.redirect('/')
+        }
+      })
+    } else {
+      const {
+        type,
+        photo,
+        description,
+        availability,
+        phone,
+        age,
+        reviews
+      } = request.body
+
+      console.log(reviews, 'guide')
+      addGuide(
+        type,
+        photo,
+        description,
+        availability,
+        phone,
+        age,
+        reviews,
+        name,
+        email,
+        hash,
+        (err, res) => {
+          if (err) {
+            console.log(err)
+          } else {
+            response.redirect('/')
+          }
+        }
+      )
+    }
   })
 }
