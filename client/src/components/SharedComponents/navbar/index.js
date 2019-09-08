d4dc1d1d4e353cda8c237672acc1d84eabe35575
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-09-08 09:11:28 +0300

    made navbar and menu component

---
+++ client/src/components/SharedComponents/navbar/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import './navbar.css'
+import logo from '../../../assest/logo.png'
+
+class NavBar extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar">
+          <div className="dropdown">
+            <img
+              alt="menu"
+              className="menu"
+              src="https://www.malavasig.it/WP/wp-content/uploads/2017/09/mobile_menu.png"
+            />
+            <div className="dropdown-content">
+              <a href="/">Home</a>
+              <a href="/guides">Guide</a>
+              <a href="/places">Places</a>
+              <a href="/arabic-words">Arabic Words</a>
+            </div>
+          </div>
+          <img alt="logo" src={logo} />
+          <h1>HEBRON MUST-SEE</h1>
+        </nav>
+      </React.Fragment>
+    )
+  }
+}
+
+export default NavBar
