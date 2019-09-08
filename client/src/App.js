be227ffedb7d673b4b0619306c4a3e413d04709a
Sahar Adem <sahar.adem95@gmail.com>
2019-09-08 14:03:08 +0300

    editing list of item state

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,51 @@
-import React from "react"
+import React, { Component } from "react"
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom"
 import Start from "./components/Start"
 import Places from "./components/Places"
 import Guides from "./components/Guides"
+import OnePlace from "./components/OnePlace"
 import "./App.css"
+import axios from "axios"
 
-const App = () => {
-  return (
-    <div>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Start} />
+class App extends Component {
+  state = {
+    listOfItems: []
+  }
 
-          <Route exact path="/places" component={Places} />
+  componentDidMount() {
+    axios.get(`/api/places`).then(result => {
+      this.setState({ listOfItems: result.data })
+    })
+  }
 
-          <Route exact path="/guides" component={Guides} />
-        </Switch>
-      </Router>
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Start} />
+            <Route
+              exact
+              path="/places"
+              render={() => <Places listOfItems={this.state.listOfItems} />}
+            />
+            <Route exact path="/guides" component={Guides} />
+            <Route
+              exact
+              path="/places/:id"
+              render={props => (
+                <OnePlace
+                  title={`Props through render`}
+                  listOfItems={this.state.listOfItems}
+                  {...props}
+                />
+              )}
+            />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App
