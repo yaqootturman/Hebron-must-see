83eb88cb501dd9b1cfb474d5e7f32a18ec131e82
Sahar Adem <sahar.adem95@gmail.com>
2019-09-08 14:26:03 +0300

    #21, solving issues, adding console.log statement in dbBuild.js

--- server/database/dbBuild.js
+++ server/database/dbBuild.js
@@ -4,6 +4,7 @@ const dbConnection = require("./dbConnection")
 const sql = fs.readFileSync(`${__dirname}/dbBuild.sql`).toString()
 
 dbConnection.query(sql, (err, res) => {
+  console.log("database is connected successfully")
   if (err) {
     throw err
   }
