8245810b7e58eeb91d5e62243ab54a96de71e217
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-09-08 09:12:11 +0300

    add navbar on it

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import React from "react"
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom"
-import Start from "./components/Start"
-import Places from "./components/Places"
-import Guides from "./components/Guides"
-import "./App.css"
+import React from 'react'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import Start from './components/Start'
+import Places from './components/Places'
+import Guides from './components/Guides'
+import NavBar from './components/SharedComonents/navbar'
+import './App.css'
 
 const App = () => {
   return (
     <div>
+      <NavBar />
       <Router>
         <Switch>
           <Route exact path="/" component={Start} />
