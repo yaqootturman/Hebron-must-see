08c4f1aaa14211d0768de2dcd028272fab49c7be
Sahar Adem <sahar.adem95@gmail.com>
2019-09-09 16:50:44 +0300

    merge with master

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const { getPlaces } = require("./places")
+const { login } = require("./login")
 const { getGuides } = require("./guides")
+const { isAuthenticated } = require("../middleware/auth")
 
-router.get("/places", getPlaces)
-router.get("/guides", getGuides)
+router.get("/places", getPlaces, isAuthenticated)
+router.post("/login", login)
+// router.get("/guides", getGuides)
 
 module.exports = router
