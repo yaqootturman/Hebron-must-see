7b216f64e5c4f89ba7adafa650aa842727dac693
Sahar Adem <sahar.adem95@gmail.com>
2019-09-09 16:54:04 +0300

    merge with master

---
+++ server/controllers/login.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt')
+const { sign } = require('jsonwebtoken')
+const { getPassword } = require('../database/queries/getPassword')
+
+const { SECRET } = process.env
+
+const createToken = (email, secret) => {
+  return sign({ email }, secret)
+}
+
+exports.login = (req, res) => {
+  const { email, password } = req.body
+
+  getPassword(email, (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      if (result) {
+        const hash = result.password
+        bcrypt
+          .compare(password, hash)
+          .then(() => createToken(email, SECRET))
+          .then(token => {
+            res
+              .cookie('token', token, { maxAge: 900000, httpOnly: true })
+              .json({ status: 'success', token })
+          })
+          .catch(err => console.log(err))
+      } else {
+        res
+          .status(200)
+          .json({ message: 'incorrect email or password', status: 'failed' })
+      }
+    }
+  })
+}
