07c6ea888e32d78e82ae437da488cbf5ca0b7c50
Sahar Adem <sahar.adem95@gmail.com>
2019-09-10 16:41:55 +0300

    editing list of places style

--- client/src/components/ListOfItems/index.js
+++ client/src/components/ListOfItems/index.js
@@ -10,32 +10,46 @@ class ListOfItems extends Component {
 
     return (
       <div>
-        {
-          <ul className="ItemsContainer ">
-            {this.props.listOfItems.map((element) => (
-              <li key={element.id} className="listCards">
+        {type === 'places' ? (
+          <ul className="places-container">
+            {this.props.listOfItems.map(element => (
+              <li key={element.id} className="list-places-cards">
                 <div>
-                  {type === 'places' ? (
-                    <Link to={`/${type}/${element.place_id}`}>
-                      <div className="place-card">
-                        <h3 className="place-name">{element.name}</h3>
-                        <img src={element.image1} alt="not loaded" className="place-image1" />
-                      </div>
-                    </Link>
-                  ) : (
-                    <Link to={`/${type}/${element.guide_id}`}>
-                      <div className="guide-card">
-                        <img src={element.photo} alt="not loaded" className="guide-image " />
-                        <h3 className="guide-name">{element.name}</h3>
-                        <h3 className="guide-phone">{element.phone}</h3>
-                      </div>
-                    </Link>
-                  )}
+                  <Link to={`/${type}/${element.place_id}`}>
+                    <div className="place-card">
+                      <h3 className="place-name">{element.name}</h3>
+                      <img
+                        src={element.image1}
+                        alt="not loaded"
+                        className="place-image1"
+                      />
+                    </div>
+                  </Link>
                 </div>
               </li>
             ))}
           </ul>
-        }
+        ) : (
+          <ul className="ItemsContainer ">
+            {this.props.listOfItems.map(element => (
+              <li key={element.id} className="listCards">
+                <div>
+                  <Link to={`/${type}/${element.guide_id}`}>
+                    <div className="guide-card">
+                      <img
+                        src={element.photo}
+                        alt="not loaded"
+                        className="guide-image "
+                      />
+                      <h3 className="guide-name">{element.name}</h3>
+                      <h3 className="guide-phone">{element.phone}</h3>
+                    </div>
+                  </Link>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     )
   }
--- client/src/components/OnePlace/index.js
+++ client/src/components/OnePlace/index.js
@@ -8,15 +8,15 @@ class OnePlace extends Component {
     return (
       <div>
         <div>
-          <img src={`${place.image1}`} />
+          <img src={`${place.image1}`} className="place-image" />
         </div>
         <div className="place-information">
-          <h2 className="place-name">{place.name}</h2>
+          <h2 className="place-name-description">{place.name}</h2>
 
           <h2 className="place-availibility">{place.availability}</h2>
           <h2 className="place-price">{place.price}</h2>
           <p className="place-description">{place.description}</p>
-          <div>
+          <div className="description-images">
             <img src={`${place.image2}`} class="description-image" />
             <img src={`${place.image3}`} class="description-image" />
           </div>
