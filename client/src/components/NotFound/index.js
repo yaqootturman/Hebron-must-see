101f310c2353c402ca27ff8359d96a96b81600d2
Sahar Adem <sahar.adem95@gmail.com>
2019-09-11 16:14:42 +0300

    adding page not found handler

---
+++ client/src/components/NotFound/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './style.css'
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>
+        OOPs! <br />
+      </h1>
+      <h5 classNAme="not-found-message">
+        We can't find the page you are looking for, you can go to home page to
+        find details <span> </span>
+        <a classeName="not-found-anchor" href="/home">
+          Home
+        </a>
+      </h5>
+    </div>
+  )
+}
+export default NotFound
