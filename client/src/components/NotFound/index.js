a4e03c769193114da8750e67c9c348c3507a0237
Sahar Adem <sahar.adem95@gmail.com>
2019-09-11 16:24:49 +0300

    deleting break in html

--- client/src/components/NotFound/index.js
+++ client/src/components/NotFound/index.js
@@ -4,10 +4,8 @@ import './style.css'
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h1>
-        OOPs! <br />
-      </h1>
-      <h5 classNAme="not-found-message">
+      <h1>OOPs!</h1>
+      <h5 className="not-found-message">
         We can't find the page you are looking for, you can go to home page to
         find details{' '}
         <a classeName="not-found-anchor" href="/home">
