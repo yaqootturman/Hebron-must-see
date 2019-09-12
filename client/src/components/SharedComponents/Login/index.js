e65741be942de803da0a48da7f5158ec5f4529f2
Rand Inaim <rand.inaim95@gmail.com>
2019-09-12 12:13:17 +0300

    fixed login link

--- client/src/components/SharedComponents/Login/index.js
+++ client/src/components/SharedComponents/Login/index.js
@@ -8,12 +8,12 @@ class Login extends Component {
     password: ''
   }
 
-  handleChange = event => {
+  handleChange = (event) => {
     const { value } = event.target
     this.setState({ [event.target.name]: value })
   }
 
-  submit = event => {
+  submit = (event) => {
     event.preventDefault()
     const { email, password } = this.state
     axios
@@ -21,14 +21,14 @@ class Login extends Component {
         email,
         password
       })
-      .then(res => {
+      .then((res) => {
         if (res.data.status === 'failed') {
           alert(res.data.message)
         } else {
           this.props.history.goBack()
         }
       })
-      .catch(err => console.log(err.status))
+      .catch((err) => console.log(err.status))
   }
 
   render() {
@@ -36,14 +36,7 @@ class Login extends Component {
     return (
       <form className="loginForm">
         <label htmlFor="email">
-          <input
-            placeholder="Email"
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-          />
+          <input placeholder="Email" type="email" id="email" name="email" value={email} onChange={this.handleChange} />
         </label>
         <label htmlFor="password">
           <input
@@ -57,15 +50,9 @@ class Login extends Component {
         </label>
         <p>
           If you dont have an account please click
-          <a href="/singup">SingUp</a>
+          <a href="/signup">SingUp</a>
         </p>
-        <button
-          className="loginButton"
-          id="login"
-          type="submit"
-          value="Submit"
-          onClick={this.submit}
-        >
+        <button className="loginButton" id="login" type="submit" value="Submit" onClick={this.submit}>
           Login
         </button>
       </form>
