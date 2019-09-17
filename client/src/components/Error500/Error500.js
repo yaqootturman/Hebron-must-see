e9ab1ccdb7980f688967cb2ee2bd3ac1939515fb
95Rawan <youlostme17@hotmail.com>
2019-09-17 17:01:02 +0300

    Removed unnecessary fragment and styled the image

--- client/src/components/Error500/Error500.js
+++ client/src/components/Error500/Error500.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react'
 import './style.css'
 
 const Error500 = () => (
-  <>
-    <img
-      className="error500"
-      src="https://blog.hubspot.com/hubfs/HTTP-500-Internal-Server-Error.jpg"
-      alt=""
-    />
-  </>
+  <img
+    className="error500"
+    src="https://blog.hubspot.com/hubfs/HTTP-500-Internal-Server-Error.jpg"
+    alt=""
+  />
 )
 
 export default Error500
